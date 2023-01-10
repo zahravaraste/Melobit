5f98b49fed2e98bc7c3fcb638821bd9d46f696f2
zahravaraste <varaste.zahraa@gmail.com>
2023-01-10 23:12:36 +0330

    add adapter package and adapter for recycler view

---
+++ app/src/main/java/com/example/melobit/adapter/MusicAdapter.java
@@ -0,0 +1,58 @@
+package com.example.melobit.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.melobit.R;
+import com.example.melobit.models.MusicData;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicViewHolder> {
+    Context context;
+    List<MusicData> list;
+
+    public MusicAdapter(Context context, List<MusicData> list) {
+        this.context = context;
+        this.list = list;
+    }
+    @NonNull
+    @Override
+    public MusicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MusicViewHolder(LayoutInflater.from(context).inflate(R.layout.list_music , parent , false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MusicViewHolder holder, int position) {
+
+        Picasso.get().load(list.get(position).getImage().getCover().getUrl()).into(holder.img_music);
+        holder.txt_musicName.setText(list.get(position).getTitle());
+        holder.txt_singerName.setText(list.get(position).getArtists().get(0).getFullName());
+    }
+
+    @Override
+    public int getItemCount()  {
+        return list.size();
+    }
+}
+
+class MusicViewHolder extends RecyclerView.ViewHolder {
+    ImageView img_music;
+    TextView txt_musicName, txt_singerName ;
+
+    public MusicViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        img_music = itemView.findViewById(R.id.img_music);
+        txt_musicName = itemView.findViewById(R.id.txt_musicName);
+        txt_singerName = itemView.findViewById(R.id.txt_singerName);
+    }
+}
