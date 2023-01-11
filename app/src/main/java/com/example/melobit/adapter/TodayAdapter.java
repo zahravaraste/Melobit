94f0cb877c68c822626bac483f14dd39b0790d3d
zahravaraste <varaste.zahraa@gmail.com>
2023-01-12 01:01:05 +0330

    Create adapter for top 10 day song recycler view

---
+++ app/src/main/java/com/example/melobit/adapter/TodayAdapter.java
@@ -0,0 +1,58 @@
+package com.example.melobit.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.melobit.R;
+import com.example.melobit.models.MusicData;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class TodayAdapter extends RecyclerView.Adapter<TodayMusicViewHolder> {
+    Context context;
+    List<MusicData> list;
+
+    public TodayAdapter(Context context, List<MusicData> list) {
+        this.context = context;
+        this.list = list;
+    }
+    @NonNull
+    @Override
+    public TodayMusicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TodayMusicViewHolder(LayoutInflater.from(context).inflate(R.layout.today_music , parent , false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TodayMusicViewHolder holder, int position) {
+        Picasso.get().load(list.get(position).getImage().getCover().getUrl()).into(holder.img_music);
+        holder.txt_musicName.setText(list.get(position).getTitle());
+        holder.txt_singerName.setText(list.get(position).getArtists().get(0).getFullName());
+        holder.txt_count.setText(list.get(position).getDownloadCount());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 11;
+    }
+}
+class TodayMusicViewHolder extends RecyclerView.ViewHolder {
+    ImageView img_music;
+    TextView txt_musicName, txt_singerName, txt_count ;
+
+    public TodayMusicViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        img_music = itemView.findViewById(R.id.img_music2);
+        txt_musicName = itemView.findViewById(R.id.txt_musicName2);
+        txt_singerName = itemView.findViewById(R.id.txt_singerName2);
+        txt_count = itemView.findViewById(R.id.txt_count);
+    }
+}
