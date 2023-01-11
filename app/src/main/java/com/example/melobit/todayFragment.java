3454f04259f468a9e448807250a83517a36d23cd
zahravaraste <varaste.zahraa@gmail.com>
2023-01-12 01:06:40 +0330

    Create a request and connect top 10 day recycler view to its adapter

--- app/src/main/java/com/example/melobit/todayFragment.java
+++ app/src/main/java/com/example/melobit/todayFragment.java
@@ -1,12 +1,26 @@
 package com.example.melobit;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.melobit.adapter.MusicAdapter;
+import com.example.melobit.adapter.TodayAdapter;
+import com.example.melobit.manager.RequestManager;
+import com.example.melobit.models.MusicData;
+import com.example.melobit.models.MusicResponse;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,11 +68,34 @@ public class todayFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    RecyclerView recyclerview;
+    RequestManager manager;
+    TodayAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today, container, false);
+        View view=inflater.inflate(R.layout.fragment_today, container, false);
+        recyclerview=view.findViewById((R.id.recycler_today));
+        manager = new RequestManager(getActivity());
+        manager.getTopDayMusic(listener);
+        return view;
+    }
+
+    private final ResponseListener<MusicResponse> listener = new ResponseListener<MusicResponse>() {
+        @Override
+        public void didFetch(List<MusicData> list, String status) {
+            showMusic(list);
+        }
+        @Override
+        public void didError(String status) {
+            Toast.makeText(getActivity(), status, Toast.LENGTH_SHORT).show();
+        }
+    };
+    private void showMusic(List<MusicData> list) {
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        adapter = new TodayAdapter(getActivity(), list);
+        recyclerview.setAdapter(adapter);
     }
 }
