3e6c7dd2352f3fb306d97cb57918b2c52fe2864e
zahravaraste <varaste.zahraa@gmail.com>
2023-01-12 00:59:08 +0330

    Create request for top 10 day song

--- app/src/main/java/com/example/melobit/manager/RequestManager.java
+++ app/src/main/java/com/example/melobit/manager/RequestManager.java
@@ -18,7 +18,8 @@ public class RequestManager {
             .baseUrl("https://api-beta.melobit.com/v1/")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
-    CallMusic callMusic = retrofit.create(CallMusic.class);
+
+    CallLatestSongs callMusic = retrofit.create(CallLatestSongs.class);
     public RequestManager (Context context) {
         this.context = context;
     }
@@ -42,14 +43,49 @@ public class RequestManager {
                 }
             });
         }
-
         catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    private interface CallMusic {
+
+    CallTopDaySongs callDayMusic = retrofit.create(CallTopDaySongs.class);
+    public void getTopDayMusic(ResponseListener listener) {
+
+        Call<MusicResponse> call2 = callDayMusic.callTopDayMusic();
+        try {
+            call2.enqueue(new Callback<MusicResponse>() {
+                @Override
+                public void onResponse(Call<MusicResponse> call, Response<MusicResponse> response) {
+                    if (!response.isSuccessful()){
+                        listener.didError(response.message());
+                        return;
+                    }
+                    listener.didFetch(response.body().getResults(),response.message());
+                }
+
+                @Override
+                public void onFailure(Call<MusicResponse> call, Throwable t) {
+                    listener.didError(t.getMessage());
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    private interface CallLatestSongs {
         @GET("song/new/0/11")
         Call<MusicResponse> callMusic();
+    }
+
+    private interface CallTopDaySongs {
+        @GET("song/top/day/0/100")
+        Call<MusicResponse> callTopDayMusic();
     }
 }
