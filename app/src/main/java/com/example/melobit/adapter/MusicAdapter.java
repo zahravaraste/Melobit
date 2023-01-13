f55111c92352d5e82ba26358d825029edec9218e
zahravaraste <varaste.zahraa@gmail.com>
2023-01-13 23:57:04 +0330

    Add song activity and implement itemView click listener

--- app/src/main/java/com/example/melobit/adapter/MusicAdapter.java
+++ app/src/main/java/com/example/melobit/adapter/MusicAdapter.java
@@ -1,6 +1,7 @@
 package com.example.melobit.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,15 +9,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.melobit.R;
+import com.example.melobit.SongActivity;
 import com.example.melobit.models.MusicData;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class MusicAdapter extends RecyclerView.Adapter<MusicViewHolder> {
+public class MusicAdapter extends RecyclerView.Adapter<MusicViewHolder>   {
     Context context;
     List<MusicData> list;
 
@@ -36,23 +39,29 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicViewHolder> {
         Picasso.get().load(list.get(position).getImage().getCover().getUrl()).into(holder.img_music);
         holder.txt_musicName.setText(list.get(position).getTitle());
         holder.txt_singerName.setText(list.get(position).getArtists().get(0).getFullName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Intent intent = new Intent(context,SongActivity.class);
+               context.startActivity(intent);
+                }
+            });
     }
-
     @Override
     public int getItemCount()  {
         return list.size();
     }
 }
-
 class MusicViewHolder extends RecyclerView.ViewHolder {
     ImageView img_music;
-    TextView txt_musicName, txt_singerName ;
+    TextView txt_musicName, txt_singerName;
+    CardView card_music;
 
     public MusicViewHolder(@NonNull View itemView) {
         super(itemView);
-
         img_music = itemView.findViewById(R.id.img_music);
         txt_musicName = itemView.findViewById(R.id.txt_musicName);
         txt_singerName = itemView.findViewById(R.id.txt_singerName);
+        card_music = itemView.findViewById(R.id.card_music);
     }
 }
