d72b0941e43abb8d427f2dff5c5e8618ffaca747
zahravaraste <varaste.zahraa@gmail.com>
2023-01-18 10:53:26 +0330

    Implement media player and seekBar in song activity

--- app/src/main/java/com/example/melobit/SongActivity.java
+++ app/src/main/java/com/example/melobit/SongActivity.java
@@ -1,23 +1,35 @@
 package com.example.melobit;
 
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.melobit.manager.RequestManager;
-import com.example.melobit.models.MusicData;
-import com.example.melobit.models.MusicResponse;
 import com.example.melobit.models.Song;
+import com.github.mfathi91.time.PersianDate;
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 
 public class SongActivity extends AppCompatActivity {
     ImageView img;
-    TextView txt_musicName,txt_singerName;
+    TextView txt_musicName, txt_singerName, txt_date, txt_downloadC, txt_duration;
+    SeekBar seekBar;
+    Button btn_play, btn_prev, btn_next;
+    MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +38,14 @@ public class SongActivity extends AppCompatActivity {
 
         txt_musicName = findViewById(R.id.txt_musicName3);
         txt_singerName = findViewById(R.id.txt_singerName3);
+        txt_date = findViewById(R.id.txt_date);
+        txt_downloadC = findViewById(R.id.txt_downloadC);
+        txt_duration = findViewById(R.id.txt_duration);
+        seekBar = findViewById(R.id.seekBar);
         img = findViewById(R.id.img_music3);
+        btn_next = findViewById(R.id.btn_next);
+        btn_play = findViewById(R.id.btn_play);
+        btn_prev = findViewById(R.id.btn_prev);
         String extras = getIntent().getStringExtra("id");
 
         if (extras != null) {
@@ -34,10 +53,12 @@ public class SongActivity extends AppCompatActivity {
             SongResponseListener listener = new SongResponseListener() {
                 @Override
                 public void didFetch(Song response, String status) {
-                   Picasso.get().load(response.getImage().getCover().getUrl()).into(img);
+                    Picasso.get().load(response.getImage().getCover().getUrl()).into(img);
                     txt_musicName.setText(response.getTitle());
                     txt_singerName.setText(response.getArtists().get(0).getFullName());
-
+                    txt_date.setText(response.getReleaseDate());
+                    txt_downloadC.setText(response.getDownloadCount());
+                    play(response.getAudio().getMedium().getUrl());
                 }
 
                 @Override
@@ -48,8 +69,96 @@ public class SongActivity extends AppCompatActivity {
             manager.getSong(listener, extras);
         }
 
-
-
     }
+
+    private void play(String url) {
+        mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build()
+        );
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            /*seekBar.setProgress(0);*/
+            seekBar.setMax(mediaPlayer.getDuration());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       /* SongActivity.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if(mediaPlayer!=null){
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                    txt_duration.setText(convertTomms(mediaPlayer.getCurrentPosition()+""));
+                }
+                new Handler().postDelayed(this,1000);
+            }
+        });*/
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                    txt_duration.setText(convertTomms(mediaPlayer.getCurrentPosition() + ""));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 1000);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mediaPlayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    btn_play.setBackgroundResource(R.drawable.play);
+                    mediaPlayer.pause();
+                } else {
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition());
+                    btn_play.setBackgroundResource(R.drawable.pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                btn_play.setBackgroundResource(R.drawable.play);
+                mediaPlayer.seekTo(0);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+    private String convertTomms(String duration) {
+        Long milis = Long.parseLong(duration);
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(milis) % TimeUnit.HOURS.toMinutes(1), TimeUnit.MILLISECONDS.toSeconds(milis) % TimeUnit.MINUTES.toSeconds(1));
+    }
+
 
 }
--- app/src/main/java/com/example/melobit/SongResponseListener.java
+++ app/src/main/java/com/example/melobit/SongResponseListener.java
@@ -1,9 +1,6 @@
 package com.example.melobit;
 
-import com.example.melobit.models.MusicData;
 import com.example.melobit.models.Song;
-
-import java.util.List;
 
 public interface SongResponseListener {
     void didFetch(Song response, String status);
